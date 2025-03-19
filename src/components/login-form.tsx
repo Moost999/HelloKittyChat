35a9ba165207f8689585504be40c6bf51daca411
Moost999 <geertschaffer@gmail.com>
2025-03-19 17:24:57 -0300

    vercel fix auth

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { signIn } from "next-auth/react";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
