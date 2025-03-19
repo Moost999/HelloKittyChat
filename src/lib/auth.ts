fc190e80c0e66eb42675a7c036340763d379f4fa
Moost999 <geertschaffer@gmail.com>
2025-03-19 17:00:17 -0300

    vercel fix auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from "@/lib/prisma";
 import { compare } from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
+secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: "Credentials",
